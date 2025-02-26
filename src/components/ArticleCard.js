de6edfc9ce4bc70d9944e3936cea2323df8c3dcf
aminagr <aminagrine98@gmail.com>
2025-02-26 01:48:43 +0100

    design

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -6,18 +6,20 @@ const ArticleCard = ({ title, description, imageUrl, slug, categories }) => {
   const imageSrc = imageUrl ? urlFor(imageUrl).width(600).url() : '/default-image.jpg';
 
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden transition-transform transform hover:scale-105">
-      <img src={imageSrc} alt={title} className="w-full h-56 object-cover" />
-      <div className="p-6">
-        <h3 className="text-xl font-bold mb-2 text-gray-900">{title}</h3>
-        <p className="text-sm text-gray-600 mb-4">{description}</p>
-        {category && (
-        <Link href={`/articles/${slug}`} className="text-primary font-semibold hover:underline">
-            Lire plus →
-          </Link>
-        )}
+    <Link href={`/articles/${slug}`} className="block">
+      <div className="bg-white shadow-lg rounded-lg overflow-hidden transition-transform transform hover:scale-105 cursor-pointer">
+        <img src={imageSrc} alt={title} className="w-full h-56 object-cover" />
+        <div className="p-6">
+          <h3 className="text-xl font-bold mb-2 text-gray-900">{title}</h3>
+          <p className="text-sm text-gray-500 mb-4">{description}</p>
+          {category && (
+            <span className="text-primary font-semibold hover:underline">
+              Lire plus →
+            </span>
+          )}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
