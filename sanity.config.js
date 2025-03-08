e1d44b60a5c170f3c8c6167ae52c79ade8f06136
aminagr <aminagrine98@gmail.com>
2025-03-08 22:41:34 +0100

    false

--- sanity.config.js
+++ sanity.config.js
@@ -11,14 +11,14 @@ import {structureTool} from 'sanity/structure'
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './src/sanity/env'
 import {schemaTypes} from './src/sanity/schemaTypes'
-
+import {frFRLocale} from '@sanity/locale-fr-fr'
 
 export default defineConfig({
   basePath: '/studio',
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
-  plugins: [structureTool(), visionTool()],
+ 
+  plugins: [structureTool(), visionTool(),  frFRLocale(),],
 
   schema: {
     types: schemaTypes,
