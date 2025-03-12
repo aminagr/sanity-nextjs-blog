17e94ae6e7d65119c91e61942966445677b0bef5
aminagr <aminagrine98@gmail.com>
2025-03-13 00:29:29 +0100

    hero design

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
       <div className="absolute inset-0 bg-black opacity-70"></div>
 
 
-      <div className="relative z-10 p-4 md:p-12 text-center rounded-2xl shadow-lg max-w-3xl">
+      <div className="relative z-10 p-4 md:p-12 text-center  max-w-3xl">
         <h1 className="text-3xl md:text-5xl font-bold text-white leading-tight pt-4">
           Voyagez à travers les saveurs du monde !
         </h1>
